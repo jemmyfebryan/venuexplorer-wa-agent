425d28a866c27eb404f2e279fa7e220320292051
Jemmy Febryan <danieljemmyfebryan@gmail.com>
2025-09-16 18:32:56 +0700

    book next and env

--- core/agent/handler.py
+++ core/agent/handler.py
@@ -1,18 +1,25 @@
 import httpx
 import re
+import os
 
 from core.logger import get_logger
 
 logger = get_logger(__name__)
 
-INQUIRY_URL = "http://34.87.68.224:8100/api/v1/recommendation/inquiry/whatsapp"
-BOOKING_URL = "http://34.87.68.224:8100/api/v1/recommendation/inquiry/book/{ticket_id}/{venue_id}"
+from dotenv import load_dotenv
+load_dotenv()
 
-async def chat_inquiry(user_name: str, message: str, phone_number: str, k_venue: int = 3) -> str:
+VPS_URL = os.getenv("VPS_URL")
+
+INQUIRY_URL = "{VPS_URL}/api/v1/recommendation/inquiry/whatsapp"
+BOOKING_URL = "{VPS_URL}/api/v1/recommendation/inquiry/book/{ticket_id}/{venue_id}"
+NEXT_BOOKING_URL = "{VPS_URL}/api/v1/recommendation/inquiry/whatsapp/{ticket_id}/next-recommendation"  # add phone number
+
+async def chat_inquiry(user_name: str, message: str, phone_number: str, k_venue: int = 5) -> str:
     """
     Send a WhatsApp inquiry to the recommendation API and return a formatted response.
 
-    Args:
+    Args:   
         user_name (str): The user's name
         message (str): The user's inquiry message
         phone_number (str): The user's phone number
@@ -27,8 +34,9 @@ async def chat_inquiry(user_name: str, message: str, phone_number: str, k_venue:
         "k_venue": k_venue
     }
 
+    inquiry_url = INQUIRY_URL.format(VPS_URL=VPS_URL)
     async with httpx.AsyncClient(timeout=15) as client:
-        response = await client.post(INQUIRY_URL, json=payload)
+        response = await client.post(inquiry_url, json=payload)
         # response.raise_for_status()
         
     if response.status_code != 200:
@@ -39,7 +47,7 @@ async def chat_inquiry(user_name: str, message: str, phone_number: str, k_venue:
     ticket_id = data.get("ticket_id", "N/A")
     venues = data.get("top_k_venues", [])
     if not venues:
-        return f"Ticket ID: {ticket_id}\nHi {user_name}, sorry! I couldn't find any venues matching your request."
+        return f"Ticket ID: {ticket_id}\nHi {user_name}, sorry! I couldn't find any venues matching your request. Kindly request another one or try again later."
 
     formatted_venues = []
     for idx, venue in enumerate(venues, start=1):
@@ -49,14 +57,82 @@ async def chat_inquiry(user_name: str, message: str, phone_number: str, k_venue:
             f"   📍 Location: {payload.get('location', 'N/A')}\n"
             f"   🏷️ Type: {payload.get('type', 'N/A')}\n"
             f"   ⭐ Amenities: {payload.get('amenities', 'N/A')}\n"
-            f"   📞 Contact: {payload.get('contact_name', 'N/A')} ({payload.get('contact_numb', 'N/A')})\n"
         )
 
     response_text = (
         f"🎟️ Ticket ID: {ticket_id}\n\n"
-        f"Hi {user_name}, Welcome to Venuexplorer! Based on your request, here are the top {len(formatted_venues)} venue recommendations:\n\n"
+        f"Hi {user_name}, Welcome to Venuexplorer! Based on your request, here are the top {len(formatted_venues)} venue recommendations:\n"
+        + "\n*To book a venue, please response with: book #  (for example: book 1)*"
+        + "\n*To request other venue recommendation, please response with: book next*\n\n"
         + "\n".join(formatted_venues)
-        + "\nTo book a venue, please reply with: book #  (for example: book 1)"
+        + "\n_ⓘ Kindly respond only according to the instructions._"
+    )
+
+    return response_text
+
+async def chat_inquiry_next(user_name: str, phone_number: str, last_message: str) -> str:
+    """
+    Send a WhatsApp inquiry to the recommendation API and return a formatted response.
+
+    Args:
+        user_name (str): The user's name
+        message (str): The user's inquiry message
+        phone_number (str): The user's phone number
+        k_venue (int, optional): Number of venues to recommend (default 3)
+
+    Returns:
+        str: Formatted recommendation message
+    """
+    # 1. Extract ticket_id
+    ticket_match = re.search(r"Ticket ID:\s*([A-Z0-9\-]+)", last_message)
+    if not ticket_match:
+        return "Sorry, I could not find a ticket ID in your inquiry. Kindly request another inquiry."
+
+    ticket_id = ticket_match.group(1)
+    
+    logger.info(f"Chat Inquiry Next: Ticket ID: {ticket_id}")
+    
+    # 2. Hit booking API
+    next_booking_url = NEXT_BOOKING_URL.format(
+        VPS_URL=VPS_URL,
+        ticket_id=ticket_id
+    )
+    logger.info(f"Chat Inquiry Next: next_booking_url: {next_booking_url}")
+    payload = {
+        "phone_number": phone_number
+    }
+    async with httpx.AsyncClient(timeout=15) as client:
+        response = await client.post(next_booking_url, json=payload)
+        
+    if response.status_code != 200:
+        return "Failed to request next booking. Please try again later."
+    
+    data = response.json()
+    logger.info(f"Chat Inquiry Next: response: {str(data)[:50]}")
+
+    # ticket_id = data.get("ticket_id", ticket_id)
+    venues = data.get("top_k_venues", [])
+    if not venues:
+        return f"Ticket ID: {ticket_id}\nHi {user_name}, sorry! There is no other recommendation. Please kindly request an inquiry again."
+
+    formatted_venues = []
+    for idx, venue in enumerate(venues, start=1):
+        payload = venue.get("payload", {})
+        formatted_venues.append(
+            f"{idx}. {payload.get('name')} ({payload.get('id', 'N/A')})\n"
+            f"   📍 Location: {payload.get('location', 'N/A')}\n"
+            f"   🏷️ Type: {payload.get('type', 'N/A')}\n"
+            f"   ⭐ Amenities: {payload.get('amenities', 'N/A')}\n"
+        )
+
+    response_text = (
+        f"🎟️ Ticket ID: {ticket_id}\n\n"
+        f"Hi {user_name}! Based on your request, here are the other {len(formatted_venues)} venue recommendations:\n"
+        + "\n*To book a venue, please response with: book #  (for example: book 1)*"
+        + "\n*To request other venue recommendation, please response with: book next*\n\n"
+        + "\n".join(formatted_venues)
+        + "\n_ⓘ Requesting additional venue recommendation may lead to inaccurate results._"
+        + "\n_ⓘ Kindly respond only according to the instructions._"
     )
 
     return response_text
@@ -76,33 +152,36 @@ async def book_selected_venue(selected_venue_index: int, inquiry_chat: str) -> s
     # 1. Extract ticket_id
     ticket_match = re.search(r"Ticket ID:\s*([A-Z0-9\-]+)", inquiry_chat)
     if not ticket_match:
-        return "Sorry, I could not find a ticket ID in your inquiry."
+        return "Sorry, I could not find a ticket ID in your inquiry. Kindly request another inquiry."
 
     ticket_id = ticket_match.group(1)
 
     # 2. Extract all venues
     venue_pattern = re.compile(
-        r"(\d+)\.\s+(.*?)\s+\((\d+)\)\n"
-        r"\s+📍 Location:\s*(.*?)\n"
-        r"\s+🏷️ Type:\s*(.*?)\n"
-        r"\s+⭐ Amenities:\s*(.*?)\n"
-        r"\s+📞 Contact:\s*(.*?)\s+\((.*?)\)",
+        r"(\d+)\.\s+([^\n]+?)\s+\((\d+)\)\r?\n"
+        r".*?📍 Location:\s*(.*?)\r?\n"
+        r".*?🏷️ Type:\s*(.*?)\r?\n"
+        r".*?⭐ Amenities:\s*(.*?)\r?\n",
         re.DOTALL,
     )
     venues = venue_pattern.findall(inquiry_chat)
 
     if not venues or selected_venue_index < 1 or selected_venue_index > len(venues):
-        return "❌ Invalid venue selection. Please try again."
+        return "Invalid venue selection. Please try request an inquriy again or try again later."
 
     # 3. Get selected venue
-    _, name, venue_id, _, _, _, _, _ = venues[selected_venue_index - 1]
+    selected_venue = venues[selected_venue_index - 1]
+    _, name, venue_id, location, venue_type, amenities = selected_venue
+    logger.info(f"Book Selected Venue: selected_venues: {selected_venue}")
 
     # 4. Hit booking API
-    booking_url = BOOKING_URL.format(ticket_id=ticket_id, venue_id=venue_id)
+    booking_url = BOOKING_URL.format(VPS_URL=VPS_URL, ticket_id=ticket_id, venue_id=venue_id)
+    logger.info(f"Book Selected Venue: booking_url: {booking_url}")
     async with httpx.AsyncClient(timeout=15) as client:
         response = await client.get(booking_url)
 
+    logger.info(f"Book Selected Venue: response: {response}")
     if response.status_code == 200:
         return f"Success! The venue *{name.strip()}* ({venue_id}) has been successfully booked under ticket {ticket_id}."
     else:
-        return f"Failed to book the venue *{name.strip()}* ({venue_id}). Please try again later."
+        return f"Failed to book the venue *{name.strip()}* ({venue_id}). Please request another inquiry or try again later."
--- core/agent/main.py
+++ core/agent/main.py
@@ -1,13 +1,25 @@
 import asyncio
 import re
+import os
+import signal
 from wa_automate_socket_client import SocketClient
 from core.open_wa.listener import ChatBotHandler
-from core.agent.handler import chat_inquiry, book_selected_venue
+from core.agent.handler import (
+    chat_inquiry,
+    chat_inquiry_next,
+    book_selected_venue
+)
 from core.logger import get_logger
 
 logger = get_logger(__name__)
 
+from dotenv import load_dotenv
+load_dotenv()
+
+OPEN_WA_PORT = os.getenv("OPEN_WA_PORT")
+
 book_pattern = re.compile(r"^book (\d+)$", re.IGNORECASE)
+    
 async def extract_book_number(text: str) -> int | None:
     match = book_pattern.match(text.strip())
     if match:
@@ -16,7 +28,7 @@ async def extract_book_number(text: str) -> int | None:
 
 async def main():
     logger.info("🚀 Starting WhatsApp bot...")
-    client = SocketClient("http://172.17.0.1:8003/", api_key="my_secret_api_key")
+    client = SocketClient(f"http://172.17.0.1:{OPEN_WA_PORT}/", api_key="my_secret_api_key")
     bot = ChatBotHandler(client)
 
     @bot.on(r"^inquiry")
@@ -25,6 +37,7 @@ async def main():
             message = msg["data"].get("body", "")
             user_name = msg["data"].get("sender").get("name")
             phone_number = msg["data"].get("from").split("@")[0]
+            logger.info(f"Book Venue! message: {message}, user_name: {user_name}, phone_number: {phone_number}")
             response = await chat_inquiry(
                 user_name=user_name,
                 message=message,
@@ -41,6 +54,7 @@ async def main():
             user_from = msg["data"].get("from")
             # logger.info(f"User_From: {user_from}")
             # phone_number = msg["data"].get("from").split("@")[0]
+            logger.info(f"Book Venue! message: {message}, user_from: {user_from}")
             last_response = client.getMyLastMessage(user_from)
             # logger.info(f"Last_Response: {last_response}")
             book_number = await extract_book_number(text=message)
@@ -49,18 +63,42 @@ async def main():
                 inquiry_chat=last_response.get("body")
             )
             await client.sendText(msg["data"]["from"], response)
-        # except Exception as e:
-        #     try:
-        #         error_response = "I'm sorry, but I can't assist with that request. Please try again later."
-        #         await client.sendText(msg["data"]["from"], error_response)
-        #     except:
-        #         logger.info(f"Error: {str(e)}")
+            
+    @bot.on(r"^book next$")
+    async def book_next(msg, client, history):
+        # try:
+        if not msg["data"]["isGroupMsg"]:
+            message = msg["data"].get("body", "")
+            user_name = msg["data"].get("sender").get("name")
+            user_from = msg["data"].get("from")
+            phone_number = msg["data"].get("from").split("@")[0]
+            logger.info(f"Book Next! message: {message}, user_name: {user_name}, phone_number: {phone_number}")
+            last_response = client.getMyLastMessage(user_from).get("body")
+            
+            response = await chat_inquiry_next(
+                user_name=user_name,
+                phone_number=phone_number,
+                last_message=last_response
+            )
+            
+            await client.sendText(msg["data"]["from"], response)
+        
+    
 
     logger.info("✅ Bot is running. Waiting for messages...")
 
+    # Graceful shutdown handler
+    async def shutdown():
+        logger.info("🛑 Shutting down socket client...")
+        client.disconnect()
+        logger.info("✅ Socket client disconnected.")
+
+    loop = asyncio.get_running_loop()
+    for sig in (signal.SIGINT, signal.SIGTERM):
+        loop.add_signal_handler(sig, lambda: asyncio.create_task(shutdown()))
+    
     # Keep the loop alive forever
     await asyncio.Event().wait()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
